feed52ee0b613d6dd3706e21fc05828096124bf7
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2024-02-18 10:10:38 +0530

    add viewmodel

--- app/src/main/java/com/example/moviemarvel/MainActivity.kt
+++ app/src/main/java/com/example/moviemarvel/MainActivity.kt
@@ -10,8 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.moviemarvel.movieList.presentation.MovieListViewModel
 import com.example.moviemarvel.ui.theme.MovieMarvelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    val movieListViewModel = hiltViewModel<MovieListViewModel>()
                     Greeting("Android")
                 }
             }
@@ -35,12 +41,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieMarvelTheme {
-        Greeting("Android")
-    }
 }
